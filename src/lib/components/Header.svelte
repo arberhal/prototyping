1177f58b1320261348f89460a6c824d0d4bb167d
arberhal <arberhalitix@gmail.com>
2025-05-22 23:27:18 +0200

    initial commit for header, without logic

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,8 +1,15 @@
 <script>
-
+	function alertFunction() {
+		alert('Button clicked!');
+	}
 </script>
 
-
-<header class="w-full h-[50px] bg-black fixed">
-
+<header class="fixed top-0 left-0 z-50 flex h-16 w-full items-center px-4 text-white shadow">
+	<h1 class="text-xl font-bold">My Music App</h1>
+	<button
+		on:click={alertFunction}
+		class="ml-auto rounded bg-white px-3 py-1 text-blue-600 hover:bg-gray-100"
+	>
+		Shuffle
+	</button>
 </header>
